a55b302417edd274edf1096290bd736ba9ae2948
unknown <zkurtyak@egp.com.ua>
2021-08-28 13:41:32 +0300

    feat: update on landing pahe

--- components/interface/Banner/Banner.tsx
+++ components/interface/Banner/Banner.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 import styled from 'styled-components';
+import Flex from "../../layout/Flex/Flex";
 
 export type BannerProps = {
   bg: string;
 }
 
-const StyledBanner = styled.div<BannerProps>`${({ theme }) => `
-  
+const StyledBanner = styled(Flex)`${({theme}) => `
+  background-color: ${theme.colors.BLACK};
+  color: ${theme.colors.WHITE};
+  padding: 39px 0 52px 0;
 `}`;
 
 export default StyledBanner;
