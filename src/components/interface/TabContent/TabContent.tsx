fa5abb8b9e0d365f75c512d4e5d4c2f7c91d8d8d
Zoltan Kurtyak <zoltankurtyak@Zoltans-MacBook-Air.local>
2022-11-28 23:20:25 +0200

    #patch update TabContent

--- src/components/interface/TabContent/TabContent.tsx
+++ src/components/interface/TabContent/TabContent.tsx
@@ -6,18 +6,20 @@ import MenuLink from "../MenuLink";
 export type TabContentProps = {
   tabs: Array<{ title: string, id: string }>;
   content: Array<any>;
+  fullWidth?: boolean;
+  justify?: "center" | "flex-start" | "flex-end";
 };
 
 const StyledTabContent = styled(Flex)`
   ${({theme}) => `
+  background-color: ${theme.colors.BLACK};
+  color: ${theme.colors.WHITE};
   font-weight: 600;
   font-size: 14px;
   line-height: 22px;
   letter-spacing: -0.15px;
   height: 50px;
   gap: 32px;
-  justify-content: center;
-  color: #151515;
   border-bottom: 1px solid #E3E3E3;
   
   ${theme.media.sm} {
@@ -30,30 +32,52 @@ const StyledTabContent = styled(Flex)`
 `}
 `;
 
+const StyledTabs = styled(Flex)`
+${({ theme}) => `
+  background-color: ${theme.colors.BLACK};
+  color: ${theme.colors.WHITE};
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    color: ${theme.colors.WHITE};
+
+  }
+
+  svg {
+    fill: ${theme.colors.WHITE}
+  }
+`}`
+
 const StyledTab = styled(MenuLink)`
   ${({theme}) => `
-    color: #5F5F5F;
+    color: ${theme.colors.WHITE};
     display: flex;
     height: 100%;
     align-items: center;
     padding: 0;
+    border-bottom: 2px solid ${theme.colors.WHITE};
+    opacity: 0.5;
   
   &:hover {
-    color: #151515;
+    opacity: 1;
   }
   &.active {
-    color: #151515;
-    border-bottom: 2px solid #151515;
+    opacity: 1;
+    color: ${theme.colors.WHITE};
+    border-bottom: 2px solid ${theme.colors.WHITE};
   }
 `}
 `;
 
-const TabContent = ({tabs, content}: TabContentProps) => {
+const TabContent = ({tabs, content, fullWidth=false, justify="center"}: TabContentProps) => {
   const [currentTab, setCurrentTab] = useState(0);
 
   return (
     <Flex flexFlow="column" align="flex-start">
-      <StyledTabContent>
+      <StyledTabContent fullWidth={fullWidth} justify={justify}>
         {tabs.map(({id, title}, index) => (
           <StyledTab
             as="h4"
