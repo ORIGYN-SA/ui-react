526485532197f4b748160332779ec7ae1b08c1ff
Zoltan Kurtyak <zoltankurtyak@Zoltans-MacBook-Air.local>
2022-12-07 15:21:09 +0200

    #patch NavbarBGfix

--- src/components/interface/Navbar/Navbar.stories.tsx
+++ src/components/interface/Navbar/Navbar.stories.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
 import Navbar from "./Navbar";
-import Home from "./MockSlides/Home";
-import Governance from "./MockSlides/Governance";
-import Wallet from "./MockSlides/Wallet";
 import HomeIcon from "../../../components/icons/home";
 import GovernanceIcon from "../../../components/icons/governance";
-import ThemeIcon from "../../../components/icons/theme";
 import WalletIcon from "../../../components/icons/wallet";
 
 export default {
